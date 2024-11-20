bf5f192159cfdf8e31a018a39f557d61b676d7f8
Bina <binachachko@gmail.com>
2024-11-21 01:28:58 +0200

    Game final improvement

---
+++ src/play/GameLogic.java
@@ -0,0 +1,24 @@
+public interface GameLogic {
+    int numberOfTries = 0;
+    boolean hasGuessed = false;
+
+        System.out.println("Я загадав число від " + min + " до " + max + ". Спробуйте його відгадати!");
+
+        while (!hasGuessed) {
+        System.out.print("Введіть своє число: ");
+        int guess = player.makeGuess(min, max);
+        numberOfTries++;
+
+        if (guess < min || guess > max) {
+            System.out.println("Число має бути в межах від " + min + " до " + max + ". Спробуйте ще раз.");
+        } else if (guess < numberToGuess) {
+            System.out.println("Загадане число більше.");
+        } else if (guess > numberToGuess) {
+            System.out.println("Загадане число менше.");
+        } else {
+            System.out.println("Ви вгадали число! Це було " + numberToGuess);
+            System.out.println("Кількість спроб: " + numberOfTries);
+            hasGuessed = true;
+        }
+    }
+}
